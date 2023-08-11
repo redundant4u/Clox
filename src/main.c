554e627f380ab97fc51782a29b9b1fa418d3ba05
redundant4u <physiogel@pusan.ac.kr>
2023-08-12 05:51:25 +0900

    feat: add vm & arithmetic operator

--- src/main.c
+++ src/main.c
@@ -1,20 +1,44 @@
 #include "chunk.h"
 #include "common.h"
 #include "debug.h"
+#include "vm.h"
 
 int main(int argc, const char *argv[]) {
+    initVM();
+
     Chunk chunk;
 
     initChunk(&chunk);
 
+    // var a = 1.2
     int constant = addConstant(&chunk, 1.2);
-
     writeChunk(&chunk, OP_CONSTANT, 123);
     writeChunk(&chunk, constant, 123);
+
+    // var b = 3.4
+    constant = addConstant(&chunk, 3.4);
+    writeChunk(&chunk, OP_CONSTANT, 123);
+    writeChunk(&chunk, constant, 123);
+
+    // a + b
+    writeChunk(&chunk, OP_ADD, 123);
+
+    // var c = 5.6
+    constant = addConstant(&chunk, 5.6);
+    writeChunk(&chunk, OP_CONSTANT, 123);
+    writeChunk(&chunk, constant, 123);
+
+    // (a + b) / c
+    writeChunk(&chunk, OP_DIVIDE, 123);
+
+    // -((a + b) / c)
+    writeChunk(&chunk, OP_NEGATE, 123);
 
     writeChunk(&chunk, OP_RETURN, 123);
 
     disassembleChunk(&chunk, "test chunk");
+    interpret(&chunk);
+    freeVM();
     freeChunk(&chunk);
 
     return 0;
